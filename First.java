971fc9140056b4a49014c79777e7e2f544ccfd09
ridvan kaplan <rkkaplanhny@gmail.com>
2022-10-29 18:26:57 +0300

    deneme2

--- First.java
+++ First.java
@@ -1,6 +1,9 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.println("asfasfasf");
+        System.out.println("asfasasfasdfasdfasdfasf");
+
     }
 
 }
